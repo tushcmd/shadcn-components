58c7f1a503e9bd1be73c6fe3e5b851f0d80aec9c
tushcmd <muturidavid854@gmail.com>
2024-05-15 00:39:14 +0300

    complete

--- .history/app/page_20240515003121.tsx
+++ .history/app/page_20240515003121.tsx
@@ -3,7 +3,7 @@ import { ProfileForm } from "@/components/profile-form";
 export default function Home() {
   return (
     //form in shadcn
-    <div className="flex flex-col items-center justify-center p-10">
+    <div className="flex flex-col items-center justify-center">
       <ProfileForm />
     </div>
   );
